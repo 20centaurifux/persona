32fcc8d8dc9114b990cb23c3dae75d91e2f5f22b
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2026-08-08 19:07:56 +0200

    Relicense project under EPL-2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject de.dixieflatline/persona "0.1.0-SNAPSHOT"
   :description "A lightweight hierarchical authorization library"
   :url "https://github.com/20centaurifux/persona"
-  :license {:name "AGPLv3.0"
-            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [de.dixieflatline/smuk "0.1.0-SNAPSHOT"]]
   :target-path "target/%s"
